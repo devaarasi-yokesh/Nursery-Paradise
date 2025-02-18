7bfbec7eba6ac563300829038c18a9834edc8508
devaarasi-yokesh <160761753+devaarasi-yokesh@users.noreply.github.com>
2025-02-18 21:39:56 +0530

    Update LandingPage.jsx

--- Main/LandingPage.jsx
+++ Main/LandingPage.jsx
@@ -12,7 +12,7 @@ return(
     <>
     <section className="landingPageContainer">
             <div className="header">
-                <h1> Conference Expense Planner</h1>
+                <h1> Paradise Nursery</h1>
                 <p>Plan your next major event with us!</p>
                 <button className="linkToProductPage" onClick={handleClick}>Get Started</button>
             </div>
@@ -21,15 +21,11 @@ return(
 Autem facilis voluptates doloremque itaque distinctio cumque maiores possimus officiis 
                      optio accusantium! Minima temporibus culpa at dignissimos numquam repellat quisquam 
                      tenetur quibusdam!Inventore modi impedit.
-                            beatae illum aliquam? </p> <br /> 
+                            beatae illum aliquam? </p> 
                             <p>Vel eligendi, esse, corporis qui 
                             quasi totam reiciendis, provident libero dolor doloribus 
                             et odio veniam soluta ea maiores! Est optio odio nesciunt 
-                            exercitationem vel libero. </p> <br />
-                     Deleniti eos cum molestiae
-                     eum aliquid doloribus laudantium id mollitia
-                     ea! <br />
-                     <p> Corporis.Temporibus quae reprehenderit porro est
+                            exercitationem vel libero. </p><p> Corporis.Temporibus quae reprehenderit porro est
                          iure. Assumenda rerum, eveniet modi, repellat tenetur ad.</p>
             </div>
         </section>
