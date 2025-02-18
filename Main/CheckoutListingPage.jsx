e9ce95c8b4dcefaea8cf0dcdac79fc89cd901f76
devaarasi-yokesh <160761753+devaarasi-yokesh@users.noreply.github.com>
2025-02-18 21:40:50 +0530

    Update CheckoutListingPage.jsx

--- Main/CheckoutListingPage.jsx
+++ Main/CheckoutListingPage.jsx
@@ -1,33 +1,28 @@
 import { useState } from "react"
 
-function Element(props){
-    return(
-        <>
-        <img src={props.img} alt="" />
-                <p>{props.name}</p>
-                <p>{props.cost*props.count}</p>
-                <p><span onClick={()=> {props.setCount((prev)=> prev+1)}}>+</span>{props.count} <span onClick={()=> {props.setCount((prev)=> prev-1)}}>-</span> </p>      
-        </>
-    )
-}
 
 
-export default function CheckoutPage(props){
-console.log(props.selectedPlants)
+
+export default function CheckoutPage({cart}){
+const total = cart.reduce((sum,item) => sum + (item.cost * item.quantity), 0);
+
     return(
-        <>
+        <div className="cartPage">
         <h1>Shopping Cart</h1>
         
-        {props.selectedPlants.map((data)=> {
-            return 
-            (
-                <>
-                 <Element count={props.count} setCount={props.setCount} img={data[3]} name={data[1]} cost={data[0]}/>
-                </>
-            )
-            
-        })}
-        <p>{props.totalCost}</p>
-        </>
+       {cart.map((item) => {
+        return (
+            <div className="item">
+            <h4>{item.name}</h4>
+            <img src={item.img} alt="" />
+            <span>
+            <p>Quantity: {item.quantity}</p>
+            <p>Total: ${(item.cost * item.quantity).toFixed(2)}</p>
+            </span>
+            </div>
+        )
+       })}
+        <p className="total">Total: ${total.toFixed(2)}</p>
+        </div>
     )
 }
