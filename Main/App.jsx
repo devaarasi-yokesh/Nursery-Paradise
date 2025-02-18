bedd162852543b8c063b59dff6c17d34d7370397
devaarasi-yokesh <160761753+devaarasi-yokesh@users.noreply.github.com>
2025-02-18 21:40:26 +0530

    Update App.jsx

--- Main/App.jsx
+++ Main/App.jsx
@@ -6,20 +6,43 @@ import './App.css'
 
 function App() {
   const [showProductPage, setShowProductPage] = useState(false);
-  const [count, setCount] = useState(0);
-  const [selectedPlants, setSelectedPlants] = useState([]);
-  const [totalCost, setTotalCost] = useState(0);
+  const [showLandingPage, setShowLandingPage] = useState(true);
+  const [showCheckoutPage, setShowCheckoutPage] = useState(false);
+  const [cart, setCart] = useState([]);
+  
+  const [selectedPlants, setSelectedPlants] = useState([
+    {id:1, img:"./src/assets/plant1.jpg", name:'SpiderPlant',about:'Same air purifier in effiecient way', cost:10},
+    {id:2, img:"./src/assets/plant2.jpg", name:'Pathos',about:'Purifies the air in a efficient way', cost:5},
+    {id:3, img:"./src/assets/plant3.jpg", name:'SpiderPlant',about:'Same air purifier in effiecient way', cost:10},
+    {id:4, img:"./src/assets/plant2.jpg", name:'Pathos',about:'Purifies the air in a efficient way', cost:5},
+    {id:5, img:"./src/assets/plant3.jpg", name:'SpiderPlant',about:'Same air purifier in effiecient way', cost:10},
+    ]);
 
-  // const k = selectedPlants.map((data)=> {
-  //   const val = data.reduce((total,num)=>{  total+num},0);
-  //   return val
-  // });
-  // console.log(k);
+ 
+
+ const addToCart = (plant,quantity) =>{
+    setCart(prevCart => {
+      const existingItem = prevCart.find(item => item.id === plant.id)
+      if(existingItem){
+        return prevCart.map(item => item.id === plant.id ? {...item, quantity: item.quantity + quantity}: item);
+      }
+      return [...prevCart, {...plant, quantity}];
+    });
+ };
+ 
+
   return (
     <>
-      {!showProductPage && <LandingPage setShowProductPage={setShowProductPage} count={count} setCount={setCount}/>} 
-      {showProductPage && <ProductListingPage setShowProductPage={setShowProductPage} count={count} setCount={setCount} setSelectedPlants={setSelectedPlants}/>} 
-      <CheckoutPage selectedPlants={selectedPlants} count={count} setCount={setCount} totalCost={totalCost}/>
+    <nav className='navigationBar'>
+      <ul>
+        <li onClick={() => {setShowLandingPage(true);setShowCheckoutPage(false);setShowProductPage(false)}}>Home</li>
+        <li onClick={() => {setShowProductPage(true);setShowLandingPage(false);setShowCheckoutPage(false)}}>Plants</li>
+        <li onClick={() => {setShowCheckoutPage(true);setShowLandingPage(false);setShowProductPage(false)}}>Cart</li>
+      </ul>
+    </nav>
+      {showLandingPage && <LandingPage setShowProductPage={setShowProductPage} cart={cart} setCart={setCart}/>} 
+      {showProductPage && <ProductListingPage addToCart={addToCart} selectedPlants={selectedPlants}/>} 
+      {showCheckoutPage && <CheckoutPage selectedPlants={selectedPlants} cart={cart} setCart={setCart} />}
     </>
   )
 }
